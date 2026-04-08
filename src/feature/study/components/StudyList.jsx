65f6922881f7f517808c4a103f42f2b53e23e08d
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-04-08 17:52:29 +0900

    feat: mock 데이터 불러오기

---
+++ src/feature/study/components/StudyList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { backgroundsMockResponse } from '../../../mocks/background/backgroundMockData';
+
+function StudyList() {
+  console.log(backgroundsMockResponse);
+  return <div>StudyList</div>;
+}
+
+export default StudyList;
