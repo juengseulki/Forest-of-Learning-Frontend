ef241875892b5175421ecb32e626bfd87ce12c75
jeungseulki <jsk432726@gmail.com>
2026-04-08 13:28:56 +0900

    style: reset 및 global 스타일 + 디자인 시스템 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-import './index.css';
+import './styles/reset.css';
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
