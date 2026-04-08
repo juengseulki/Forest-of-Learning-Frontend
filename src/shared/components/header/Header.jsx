876372d7d8a727c9b8bcc8e73ec70c75f9748edf
jeungseulki <jsk432726@gmail.com>
2026-04-08 11:39:53 +0900

    feat: 공통 header 컴포넌트 생성

---
+++ src/shared/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import logoImage from '../../images/logo/logo.png';
+import './Header.css';
+
+function Header({ rightContent }) {
+  return (
+    <header className="header">
+      <div className="header__inner">
+        <Link to="/" className="header__logo-link">
+          <img src={logoImage} alt="공부의 숲" className="header__logo" />
+        </Link>
+
+        <div className="header__right">{rightContent}</div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
