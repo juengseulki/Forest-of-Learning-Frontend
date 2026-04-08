931d0d076e687ea1b1fd9510481af23491d2b6aa
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-04-08 17:42:42 +0900

    feat: 카드 레이아웃

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
+import StudyCard from '../feature/study/components/StudyCard';
+
 function HomePage() {
   return (
     <section>
-      <h1>홈 페이지</h1>
-      <p>여기에 홈 화면 내용이 들어갑니다.</p>
+      <StudyCard />
     </section>
   );
 }
