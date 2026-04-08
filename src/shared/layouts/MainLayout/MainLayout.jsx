5401e8e54e587e7d716af46559a90df7556887fb
jeungseulki <jsk432726@gmail.com>
2026-04-08 11:50:00 +0900

    feat: main layout 컴포넌트 추가

---
+++ src/shared/layouts/MainLayout/MainLayout.jsx
@@ -0,0 +1,31 @@
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
+import './MainLayout.css';
+
+function MainLayout() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const isHomePage = location.pathname === '/';
+
+  return (
+    <div className="layout">
+      <Header
+        rightContent={
+          isHomePage ? (
+            <Button onClick={() => navigate('/study/create')}>
+              스터디 만들기
+            </Button>
+          ) : null
+        }
+      />
+
+      <main className="layout__main">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export default MainLayout;
