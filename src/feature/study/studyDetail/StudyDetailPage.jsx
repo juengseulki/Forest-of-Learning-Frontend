9cf24b9e698c58e6e0ab3fb757f941683940aa9e
sebikawa <tpqlsdl150@naver.com>
2026-04-09 16:29:23 +0900

    fix: 컴포넌트 분리 작업
    +목데이터 적용

--- src/feature/study/studyDetail/StudyDetailPage.jsx
+++ src/feature/study/studyDetail/StudyDetailPage.jsx
@@ -1,76 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../../styles/StudyDetailPage.css';
 import '../../../styles/global.css';
 import { studiesMockResponse } from '../../../mocks/study/studyMockData';
-import { Link } from 'react-router-dom';
-import smileIcon from '../../../images/icon/ic_smile.svg';
-import pointIcon from '../../../shared/images/icons/ic_point.png';
-import allowRight from '../../../images/icon/ic_arrow_right.svg';
+import { useParams } from 'react-router-dom';
+import StudyInfoSection from './StudyInfoSection.jsx';
+import StudyActionButtonGroup from './actionSection/StudyActionButtonGroup';
+import StudyLinkGroup from './actionSection/StudyLinkGroup';
+import EmojiSection from './EmojiSection.jsx';
 
 function StudyDetailPage() {
-  const data = {
-    id: 123,
-    nickname: '연유', // 유저 활동명
-    name: '연우의 개발공장', // 스터디 또는 워크스페이스 이름
-    description: '매일매일 성장하는 습관을 만드는 풀스택 개발 스터디입니다.!',
-    background: 'https://images.unsplash.com/photo-1517694712202-14dd9538aa97', // 배경 이미지 URL
-    createdAt: '2026-04-08T10:00:00Z', // 생성 날짜
-    totalPoint: 310,
-  };
+  const { id } = useParams();
+
+  const [study, setStudy] = useState({});
+
+  useEffect(() => {
+    const loadStudy = async () => {
+      try {
+        //const res = await fetch(`http://localhost:4000/study/:${id}`);
+        //const data = await res.json()
+
+        const allItems = studiesMockResponse.data.items;
+        const targetStudy = allItems.find(
+          (item) => String(item.id) === String(id)
+        );
+        if (targetStudy) {
+          setStudy(targetStudy);
+        }
+      } catch (error) {
+        console.error('스터디 로딩 실패!', error);
+      }
+    };
+
+    loadStudy();
+  }, [id]);
 
   return (
     <div className="detail-wrapper">
       <div className="detail-container">
         <section className="detail-top-section">
           <div className="detail-left">
-            <span className="emoji">
-              <div className="emoji-wrap">
-                <button className="emoji-btn">🔥 9</button>
-                <button className="emoji-btn">🤩 9</button>
-              </div>
-              <button className="emoji-add-btn">
-                <img src={smileIcon} alt="이모지 추가" />
-                추가
-              </button>
-            </span>
-
-            <section className="detail-info">
-              <h1>{data.name}</h1>
-              <div className="detail-field">
-                <h3>소개</h3>
-                <p>{data.description}</p>
-              </div>
-            </section>
-            <div className="detail-point-group">
-              <h3 className="detail-field">현재까지 획득한 포인트</h3>
-              <div className="detail-point">
-                <img src={pointIcon} alt="포인트 아이콘" />
-                {data.totalPoint}P 획득
-              </div>
-            </div>
+            <EmojiSection />
+            <StudyInfoSection study={study} studyId={id} />
           </div>
           <div className="detail-right">
-            <div className="detail-action-group">
-              <button className="action-item ">공유하기 </button>
-              <button className="action-item">수정하기 </button>
-              <button className="action-item detail-delete">
-                스터디 삭제하기{' '}
-              </button>
-            </div>
-            <div className="detail-link-group">
-              <Link to="" className="link-btn">
-                오늘의 습관 <img src={allowRight} alt="오른쪽 이미지" />
-              </Link>
-              <Link to="" className="link-btn">
-                오늘의 집중 <img src={allowRight} alt="오른쪽 이미지" />
-              </Link>
-            </div>
+            <StudyActionButtonGroup />
+            <StudyLinkGroup />
           </div>
         </section>
-        <section className="detail-bottom-section">
-          <h2>습관 기록표</h2>
-          <div>아직 습관이 없어요.</div>
-        </section>
+        <HabitRecord />
       </div>
     </div>
   );
