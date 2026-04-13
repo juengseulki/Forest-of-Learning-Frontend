e537063c260477ebbdcc95677e0deb5ab07fe0aa
sebikawa32 <tpqlsdl150@naver.com>
2026-04-13 16:03:32 +0900

    refactor: icon Import 파일 추가

--- src/feature/study/studyDetail/HabitRecord.jsx
+++ src/feature/study/studyDetail/HabitRecord.jsx
@@ -1,50 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { habitRecordMockResponse } from '../../../mocks/habit/habitMockData';
-import blackPow from '../../../images/icon/ic_pow.svg';
-import emptyPow from '../../../../src/images/habit/sticker_empty.png';
-import pow01 from '../../../../src/images/habit/sticker_light_green_100_01.png';
-import pow07 from '../../../../src/images/habit/sticker_blue_100_07.png';
-import pow08 from '../../../../src/images/habit/sticker_blue_200_08.png';
-import pow09 from '../../../../src/images/habit/sticker_blue_300_09.png';
-import pow06 from '../../../../src/images/habit/sticker_green_06.png';
-import pow02 from '../../../../src/images/habit/sticker_light_green_100_02.png';
-import pow03 from '../../../../src/images/habit/sticker_light_green_100_03.png';
-import pow04 from '../../../../src/images/habit/sticker_light_mint_100_04.png';
-import pow05 from '../../../../src/images/habit/sticker_light_mint_200_05.png';
-import pow16 from '../../../../src/images/habit/sticker_pink_100_16.png';
-import pow17 from '../../../../src/images/habit/sticker_pink_200_17.png';
-import pow18 from '../../../../src/images/habit/sticker_pink_300_18.png';
-import pow10 from '../../../../src/images/habit/sticker_purple_100_10.png';
-import pow11 from '../../../../src/images/habit/sticker_purple_200_11.png';
-import pow12 from '../../../../src/images/habit/sticker_purple_300_12.png';
-import pow13 from '../../../../src/images/habit/sticker_yellow_100_13.png';
-import pow014 from '../../../../src/images/habit/sticker_yellow_200_14.png';
-import pow015 from '../../../../src/images/habit/sticker_yellow_300_15.png';
+import { HABIT_ICONS } from './habitIcons.js';
 
 function HabitRecord({ studyId }) {
   const [habits, setHabits] = useState([]);
   const [weekStart, setWeekStart] = useState('');
-
-  const completedPow = [
-    pow01,
-    pow02,
-    pow03,
-    pow04,
-    pow05,
-    pow06,
-    pow07,
-    pow08,
-    pow09,
-    pow10,
-    pow11,
-    pow12,
-    pow13,
-    pow014,
-    pow015,
-    pow16,
-    pow17,
-    pow18,
-  ];
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -84,7 +44,9 @@ function HabitRecord({ studyId }) {
           </div>
           {habits.map((habit, index) => {
             const currentCompletedIcon =
-              completedPow[index % completedPow.length];
+              HABIT_ICONS.COMPLETED_LIST[
+                index % HABIT_ICONS.COMPLETED_LIST.length
+              ];
 
             return (
               <div key={habit.habitId} className="habit-row">
@@ -98,9 +60,9 @@ function HabitRecord({ studyId }) {
                         {status === true ? (
                           <img src={currentCompletedIcon} alt="완료" />
                         ) : status === false ? (
-                          <img src={emptyPow} alt="미완료" />
+                          <img src={HABIT_ICONS.EMPTY} alt="미완료" />
                         ) : (
-                          <img src={blackPow} alt="습관 없음" />
+                          <img src={HABIT_ICONS.BLACK} alt="습관 없음" />
                         )}
                       </div>
                     );
