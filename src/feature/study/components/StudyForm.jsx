31512700d6a92276e854ade39e1c16aec621ab9f
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-04-13 09:49:27 +0900

    feat: 구조 연결

---
+++ src/feature/study/components/StudyForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Input from '../../../components/common/Input';
+
+const StudyForm = () => {
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+};
+
+export default StudyForm;
