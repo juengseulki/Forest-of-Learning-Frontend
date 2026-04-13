6b9366fc871f0c4c5ba44cdd6ce168aad4d551e3
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-04-13 10:33:36 +0900

    feat: 기본 input 작업

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import '../../styles/Input.css';
 
-function Input() {
-  return <div>Input</div>;
+function Input({
+  labelName = '닉네임',
+  placeholder = '닉네임을 입력해 주세요',
+}) {
+  return (
+    <div className="input-container">
+      <label className="input-label">{labelName}</label>
+      <div className="input-wrapper">
+        <input className="input-field" placeholder={placeholder} />
+      </div>
+    </div>
+  );
 }
 
 export default Input;
