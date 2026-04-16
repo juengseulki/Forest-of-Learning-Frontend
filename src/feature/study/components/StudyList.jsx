d1bc8598121086438d4f1c51c493a08d996e89d2
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-04-16 16:36:06 +0900

    feat: 스터디 조회

--- src/feature/study/components/StudyList.jsx
+++ src/feature/study/components/StudyList.jsx
@@ -1,5 +1,4 @@
 import '../../../styles/StudyList.css';
-
 import StudyCard from './StudyCard';
 import { studiesMockResponse } from '../../../mocks/study/studyMockData.js';
 import { pointMockResponse } from '../../../mocks/point/pointMockData.js';
@@ -7,14 +6,29 @@ import { backgroundsMockResponse } from '../../../mocks/background/backgroundMoc
 import { emojiMockResponse } from '../../../mocks/emoji/emojiMockData.js';
 import getBackgroundTheme from '../../../shared/utils/backgroundTheme.js';
 import { getStudyCardProps } from '../utils/studyUtils';
+import { getStudies } from '../../../api/studyApi.js';
+import { useEffect, useState } from 'react';
 
 function StudyList({ visibleCount }) {
-  const studies = studiesMockResponse.data.items;
+  // const studies = studiesMockResponse.data.items;
   const point = pointMockResponse.data;
   const backgrounds = backgroundsMockResponse.data.items;
   const emojiItems = emojiMockResponse.data.items;
-
+  const [studies, setStudies] = useState([]);
   const visibleStudies = studies.slice(0, visibleCount);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getStudies();
+        setStudies(data.items);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  });
 
   return (
     <div className="card-list">
