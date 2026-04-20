e4abf7af12cd68c93091405f67f5a552d8d69597
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-04-20 17:07:15 +0900

    feat: length=0일 때 페이지네이션 안보이게

--- src/feature/study/shared/modal/StudyRecordModal.jsx
+++ src/feature/study/shared/modal/StudyRecordModal.jsx
@@ -66,6 +66,7 @@ function StudyRecordModal({
           return formatDateString(logDate) === selected;
         });
         setPointLogs(filteredData);
+        console.log(data);
         setCurrentPage(1);
       } catch (error) {
         console.error(error);
@@ -83,81 +84,88 @@ function StudyRecordModal({
       onClose={onClose}
       className="study-modal__content--confirm"
     >
-      <div className="record-date-nav">
-        <button type="button" onClick={handlePrevDate}>
-          &lt;
-        </button>
+      <div className="record-container">
+        <div className="record-date-nav">
+          <button type="button" onClick={handlePrevDate}>
+            &lt;
+          </button>
 
-        <span>{formatDateString(selectedDate)}</span>
+          <span>{formatDateString(selectedDate)}</span>
 
-        <button type="button" onClick={handleNextDate}>
-          &gt;
-        </button>
-      </div>
-      <table className="record-table">
-        <thead>
-          <tr>
-            <th>번호</th>
-            <th>집중 시간</th>
-            <th>획득 포인트</th>
-            <th>시작 시간</th>
-            <th>종료 시간</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentPageLogs.length > 0 ? (
-            currentPageLogs.map((log, index) => (
-              <tr key={log.id}>
-                <td>{startIndex + index + 1}</td>
-                <td>{log.focusSession.duration}</td>
-                <td>{log.amount}point</td>
-                <td>{formatTime(log.focusSession.startedAt)}</td>
-                <td>{formatTime(log.focusSession.completedAt)}</td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="5">기록이 없습니다.</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-      <div className="record-pagination">
-        <button
-          className="record-pagination__button"
-          onClick={handlePrevPage}
-          disabled={currentPage === 1}
-        >
-          &lt;
-        </button>
-
-        <div className="record-pagination__pages">
-          {Array.from({ length: totalPages }, (_, index) => {
-            const page = index + 1;
-
-            return (
-              <button
-                key={page}
-                type="button"
-                className={`record-pagination__page ${
-                  currentPage === page ? 'record-pagination__page--active' : ''
-                }`}
-                onClick={() => handlePageClick(page)}
-              >
-                {page}
-              </button>
-            );
-          })}
+          <button type="button" onClick={handleNextDate}>
+            &gt;
+          </button>
         </div>
+        <table className="record-table">
+          <thead>
+            <tr>
+              <th>번호</th>
+              <th>집중 시간</th>
+              <th>획득 포인트</th>
+              <th>시작 시간</th>
+              <th>종료 시간</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentPageLogs.length > 0 ? (
+              currentPageLogs.map((log, index) => (
+                <tr key={log.id}>
+                  <td>{startIndex + index + 1}</td>
+                  <td>{log.focusSession.duration}</td>
+                  <td>{log.amount}P</td>
+                  <td>{formatTime(log.focusSession.startedAt)}</td>
+                  <td>{formatTime(log.focusSession.completedAt)}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="5">기록이 없습니다.</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+        {currentPageLogs.length > 0 && (
+          <div className="record-pagination">
+            <button
+              className="record-pagination__button"
+              onClick={handlePrevPage}
+              disabled={currentPage === 1}
+            >
+              &lt;
+            </button>
 
-        <button
-          className="record-pagination__button"
-          onClick={handleNextPage}
-          disabled={currentPage === totalPages}
-        >
-          &gt;
-        </button>
+            <div className="record-pagination__pages">
+              {Array.from({ length: totalPages }, (_, index) => {
+                const page = index + 1;
+
+                return (
+                  <button
+                    key={page}
+                    type="button"
+                    className={`record-pagination__page ${
+                      currentPage === page
+                        ? 'record-pagination__page--active'
+                        : ''
+                    }`}
+                    onClick={() => handlePageClick(page)}
+                  >
+                    {page}
+                  </button>
+                );
+              })}
+            </div>
+
+            <button
+              className="record-pagination__button"
+              onClick={handleNextPage}
+              disabled={currentPage === totalPages}
+            >
+              &gt;
+            </button>
+          </div>
+        )}
       </div>
+
       <div className="study-modal__actions">
         <button
           type="button"
